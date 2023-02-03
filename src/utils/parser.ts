980cd542ec2564ee192ec317fb7c7df350cb5b57
MoritzRS <moritz.r.schulz@gmail.com>
2023-02-03 17:37:40 +0100

    made colors base extensible

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -8,7 +8,7 @@ import { ColorShades, Configuration, UserConfig } from "./types";
 export function parser(config: UserConfig): Configuration {
 	// parse colors
 	const themeColors: { [key: string]: ColorShades } = {};
-	const baseColors = config.colors ?? {
+	const baseColors = {
 		primary: "hsl(259 94% 51%)",
 		secondary: "hsl(314 100% 47%)",
 		accent: "hsl(174 60% 50%)",
@@ -17,6 +17,7 @@ export function parser(config: UserConfig): Configuration {
 		warning: "hsl(43 96% 56%)",
 		error: "hsl(0 91% 71%)",
 		basis: "hsl(0 0% 100%)",
+		...(config.colors ?? {}),
 	};
 	for (let key in baseColors) {
 		const color = Color.fromCSS(baseColors[key]);
