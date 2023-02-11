a69cd4f8acff88f2ddb88e6264f9676b7895313a
MoritzRS <moritz.r.schulz@gmail.com>
2023-02-11 11:18:26 +0100

    started restructurung buttons

--- docs/src/routes/components/buttons/+page.svelte
+++ docs/src/routes/components/buttons/+page.svelte
@@ -6,15 +6,41 @@
 	<Headline>Buttons</Headline>
 
 	<div class="flex flex-wrap gap-4">
-		<button class="btn shadow">Default</button>
-		<button class="btn btn-basis shadow">Basis</button>
-		<button class="btn btn-primary shadow">Primary</button>
-		<button class="btn btn-secondary shadow">Secondary</button>
-		<button class="btn btn-accent shadow">Accent</button>
-		<button class="btn btn-info shadow">Info</button>
-		<button class="btn btn-success shadow">Success</button>
-		<button class="btn btn-warning shadow">Warning</button>
-		<button class="btn btn-error shadow">Error</button>
+		<button class="button">Default</button>
+		<button class="button button-basis">Basis</button>
+		<button class="button button-primary">Primary</button>
+		<button class="button button-secondary">Secondary</button>
+		<button class="button button-accent">Accent</button>
+		<button class="button button-info">Info</button>
+		<button class="button button-success">Success</button>
+		<button class="button button-warning">Warning</button>
+		<button class="button button-error">Error</button>
+	</div>
+
+	<hr class="my-4"/>
+
+	<div class="flex flex-wrap gap-4">
+		<button class="button button-soft-basis">Basis</button>
+		<button class="button button-soft-primary">Primary</button>
+		<button class="button button-soft-secondary">Secondary</button>
+		<button class="button button-soft-accent">Accent</button>
+		<button class="button button-soft-info">Info</button>
+		<button class="button button-soft-success">Success</button>
+		<button class="button button-soft-warning">Warning</button>
+		<button class="button button-soft-error">Error</button>
+	</div>
+
+	<hr class="my-4"/>
+
+	<div class="flex flex-wrap gap-4">
+		<button class="button button-outlined-basis">Basis</button>
+		<button class="button button-outlined-primary">Primary</button>
+		<button class="button button-outlined-secondary">Secondary</button>
+		<button class="button button-outlined-accent">Accent</button>
+		<button class="button button-outlined-info">Info</button>
+		<button class="button button-outlined-success">Success</button>
+		<button class="button button-outlined-warning">Warning</button>
+		<button class="button button-outlined-error">Error</button>
 	</div>
 </template>
 
--- src/lib/components/buttons.ts
+++ src/lib/components/buttons.ts
@@ -6,30 +6,57 @@ export function buttons(config: Configuration) {
 		/**
 		 * Base Button Settings
 		 */
-		".btn": {
-			border: `1px solid black`,
+		".button": {
+			border: `1px solid transparent`,
 			borderRadius: "4px",
 			display: "flex",
 			gap: "2px",
 			padding: "8px 16px",
 			fontWeight: "700",
+			transition: "all .15s ease-in-out",
 
-			...Object.keys(config.colors)
-				.map((color) => {
-					const background = config.colors[color][500].css();
-					const foreground =
-						config.colors[color][500].brightness() > 0.6
-							? config.colors[color].DARK.css()
-							: config.colors[color].LIGHT.css();
-					return {
-						[`&-${color}`]: {
-							borderColor: background,
-							backgroundColor: background,
-							color: foreground,
-						},
-					};
-				})
-				.reduce((a, b) => ({ ...a, ...b })),
+			"&:active:hover,&:active:focus": {
+				transform: "scale(0.95)",
+			},
+
+			"&:focus-visible": {
+				outlineOffset: "2px",
+				outline: "2px solid black",
+			},
 		},
+
+		/**
+		 * Colors and Variations
+		 */
+		...Object.keys(config.colors)
+			.map((color) => {
+				const shades = config.colors[color];
+				const background = shades[500].css();
+				const foreground = shades[500].brightness() > 0.6 ? shades.DARK.css() : shades.LIGHT.css();
+				return {
+					[`.button-${color}`]: {
+						borderColor: background,
+						backgroundColor: background,
+						color: foreground,
+
+						"&:focus-visible": {
+							outlineColor: shades[500].css(),
+						},
+					},
+
+					[`.button-soft-${color}`]: {
+						borderColor: foreground,
+						backgroundColor: foreground,
+						color: background,
+					},
+
+					[`.button-outlined-${color}`]: {
+						borderColor: background,
+						backgroundColor: "transparent",
+						color: background,
+					},
+				};
+			})
+			.reduce((a, b) => ({ ...a, ...b })),
 	};
 }
